09ad82085d67431bd548ce7130b7392c887371b2
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-08-25 11:58:12 +0900

    application.rbをカスタマイズ

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,13 @@ Bundler.require(*Rails.groups)
 
 module TsubasaYoshida
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
