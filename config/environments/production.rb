d91d3e8919c77db92f0bef764bc1f8bc5d33f5bf
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-08-29 23:32:16 +0900

    お問い合わせをいただいたら、メールで通知されるようにする(production環境版)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :domain => 'herokuapp.com',
+      :authentication => :plain,
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD']
+  }
 end
