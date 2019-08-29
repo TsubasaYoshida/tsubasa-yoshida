235a03213cea812f89fd7e096d194e2297cc2dc7
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-08-29 23:34:39 +0900

    問い合わせ通知メールの送信先アドレスを実在するものに設定する

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -4,7 +4,7 @@ class NoticeMailer < ApplicationMailer
 
   def sendmail_contact(contact)
     @contact = contact
-    mail to: "admin@example.com",
+    mail to: "tsubasayoshida0624@gmail.com",
          subject: "お問い合わせが届きました"
   end
 
