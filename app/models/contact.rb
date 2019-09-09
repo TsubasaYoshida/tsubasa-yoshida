9e43462d867f4846c48060d481653bee7e4fd239
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-09-09 22:16:15 +0900

    お問い合わせページのバリデーションを強化する

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PHONE_REGEX = /\A\d{10}\z|\A\d{11}\z/
   validates :name, :email, :telephone, :message,
             presence: true
+  validates :email,
+            format: {with: VALID_EMAIL_REGEX}, allow_blank: true
+  validates :telephone,
+            format: {with: VALID_PHONE_REGEX}, allow_blank: true
 end
