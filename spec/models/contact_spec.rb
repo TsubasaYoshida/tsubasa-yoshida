0e01012794eaf7f10ab784d7f982afb2d25f6a03
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-09-14 11:10:04 +0900

    Contactモデルのバリデーションテストを追加する

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -34,4 +34,40 @@ RSpec.describe Contact, type: :model do
     expect(@contact.errors[:message]).to include("を入力してください")
   end
 
+  it "「貴社名」が256文字以上であれば無効な状態であること" do
+    @contact.company = 'a' * 256
+    @contact.valid?
+    expect(@contact.errors[:company]).to include("は255文字以内で入力してください")
+  end
+
+  it "「お名前」が256文字以上であれば無効な状態であること" do
+    @contact.name = 'a' * 256
+    @contact.valid?
+    expect(@contact.errors[:name]).to include("は255文字以内で入力してください")
+  end
+
+  it "「メールアドレス」が256文字以上であれば無効な状態であること" do
+    @contact.email = 'a' * 256
+    @contact.valid?
+    expect(@contact.errors[:email]).to include("は255文字以内で入力してください")
+  end
+
+  it "「メールアドレス」に不正な文字が含まれると無効な状態であること" do
+    invalid_addresses = %w[Aa1+-.~@a1-..com Aa1+-.@a1-.~.com Aa1+-.@a1-..com~]
+    invalid_addresses.each do |invalid_address|
+      @contact.email = invalid_address
+      @contact.valid?
+      expect(@contact.errors[:email]).to include("は不正な値です")
+    end
+  end
+
+  it "「電話番号」が不正なフォーマットであれば無効な状態であること" do
+    invalid_telephone_numbers = %w[1234567890a 123456789 123456789012]
+    invalid_telephone_numbers.each do |invalid_telephone_number|
+      @contact.telephone = invalid_telephone_number
+      @contact.valid?
+      expect(@contact.errors[:telephone]).to include("は不正な値です")
+    end
+  end
+
 end
