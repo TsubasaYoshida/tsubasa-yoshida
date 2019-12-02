dc65a41ea8fa5dacd455ecbf6298ac91ae7b1ace
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-12-02 22:22:57 +0900

    Heroku環境で500エラーになる事象に対処する

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,7 @@ Bundler.require(*Rails.groups)
 
 module TsubasaYoshida
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.paths.add 'lib', eager_load: true
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
