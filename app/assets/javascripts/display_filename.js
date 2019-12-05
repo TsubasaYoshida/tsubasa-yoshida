e64f008313ab3673fd8fdf6b2e980b6dda36c891
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-12-05 22:45:26 +0900

    ソースコードのファイルパス表示の位置を変更する

--- app/assets/javascripts/display_filename.js
+++ app/assets/javascripts/display_filename.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', function(){
   var filename = $("p:contains('===')").text().replace('===', '');
-  $("p:contains('===')").replaceWith('<span class="filename">' + filename + '</span>');
+  $("p:contains('===')").next().prepend('<span class="filename">' + filename + '</span>');
+  $("p:contains('===')").remove();
 });
