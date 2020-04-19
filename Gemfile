7004622ded598b32e1d77ac327cbe4bdfe0ce8e9
y_tsubasa <tbs_chako@yahoo.co.jp>
2020-04-19 21:16:12 +0900

    #11 Railsバージョンを5.2.3から6.0.2.2にアップグレードする

--- Gemfile
+++ Gemfile
@@ -3,15 +3,14 @@ git_source(:github) {|repo| "https://github.com/#{repo}.git"}
 
 ruby '2.7.1'
 
-gem 'rails', '~> 5.2.3'
+gem 'rails', '6.0.2.2'
 gem 'mysql2', '>= 0.4.4', '< 0.6.0'
-gem 'puma', '~> 3.12'
-gem 'sass-rails', '~> 5.0'
-gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.2'
+gem 'puma', '~> 4.1'
+gem 'sass-rails', '>= 6'
+gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
-gem 'jbuilder', '~> 2.5'
-gem 'bootsnap', '>= 1.1.0', require: false
+gem 'jbuilder', '~> 2.7'
+gem 'bootsnap', '>= 1.4.2', require: false
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml-rails'
 gem 'bootstrap'
@@ -30,7 +29,7 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'capybara'
   gem 'selenium-webdriver'
-  gem 'webdrivers', '~> 4.0'
+  gem 'webdrivers'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,6 @@
 require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,8 +8,9 @@ Bundler.require(*Rails.groups)
 
 module TsubasaYoshida
   class Application < Rails::Application
+    config.load_defaults 6.0
+
     config.paths.add 'lib', eager_load: true
-    config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
